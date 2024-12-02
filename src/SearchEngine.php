78b722957679d1f1040f92f89ba890acf30de5f4
Elías Fernández <eliasfernandez@gmail.com>
2024-12-02 20:02:37 +0100

    Add the License to the project

--- src/SearchEngine.php
+++ src/SearchEngine.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the PHPhind package.
+ *
+ * (c) Elías Fernández Velázquez
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace SearchEngine;
 
 use SearchEngine\Index\Storage;
