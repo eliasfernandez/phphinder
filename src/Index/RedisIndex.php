e660f04705a83ded8ef6b840c4d55674983c1978
Elías Fernández <eliasfernandez@gmail.com>
2025-01-13 06:37:43 +0100

    Add Redis Support
    Using the popular library `predis/predis`, adds support to use Redis
    as the search Storage.
    
    * `RedisIndex`
    * `RedisStorage`

---
+++ src/Index/RedisIndex.php
@@ -0,0 +1,139 @@
+<?php
+
+/**
+ * This file is part of the PHPhind package.
+ *
+ * (c) Elías Fernández Velázquez
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PHPhinder\Index;
+
+use Predis\Client;
+
+class RedisIndex implements Index
+{
+    private Client $client;
+
+    /**
+     * @var array<string>
+     */
+    private array $properties;
+    private string $key;
+
+    public function __construct(string $connectionString, private readonly string $pattern, private readonly int $schemaOptions = 0)
+    {
+        $this->client = new Client($connectionString);
+    }
+
+    public function open(): void
+    {
+    }
+
+    public function close(): void
+    {
+    }
+
+    public function isCreated(): bool
+    {
+        return true;
+    }
+
+    public function isEmpty(): bool
+    {
+        return $this->getTotal() === 0;
+    }
+
+    public function create(array $properties): void
+    {
+        $this->key = $properties[0];
+        $this->properties = $properties;
+    }
+
+    public function drop(): void
+    {
+        $this->client->del(sprintf('phphinder:%s', $this->pattern));
+    }
+
+    public function set(array $search, array $data): void
+    {
+        foreach ($data as $property => $value) {
+            $this->client->hset(
+                sprintf('phphinder:%s:%s', $this->pattern, $search[$this->key]),
+                $property,
+                $value
+            );
+        }
+    }
+
+    public function insertMultiple(array $items): void
+    {
+        if (0 === count($items)) {
+            return;
+        }
+
+        foreach ($items as $item) {
+            $this->set([$this->key => $item[$this->key]], $item);
+        }
+    }
+
+    public function deleteMultiple(array $items): void
+    {
+        if (0 === count($items)) {
+            return;
+        }
+
+        foreach ($items as $value) {
+            $this->delete([$this->key => $value]);
+        }
+    }
+
+    public function truncate(): void
+    {
+        $this->drop();
+    }
+
+    public function delete(array $search): void
+    {
+        $this->client->hdel(
+            sprintf('phphinder:%s:%s', $this->pattern, current($search)),
+            $this->properties
+        );
+    }
+
+    public function find(array $search): array
+    {
+        return $this->client->hgetall(sprintf('phphinder:%s:%s', $this->pattern, current($search)));
+    }
+
+    public function findAll(array $search = null): \Generator
+    {
+        if (null === $search) {
+            $results = $this->client->keys(sprintf('phphinder:%s:*', $this->pattern));
+            $search = [$this->key => array_map(fn (string $pattern) =>
+                str_replace(
+                    sprintf('phphinder:%s:', $this->pattern),
+                    '',
+                    $pattern
+                ), $results)];
+        }
+
+        foreach ($search[$this->key] as $value) {
+            yield $this->client->hgetall(sprintf('phphinder:%s:%s', $this->pattern, $value));
+        }
+    }
+
+    public function getTotal(): int
+    {
+        return count(
+            $this->client->keys(sprintf('phphinder:%s:*', $this->pattern))
+        );
+    }
+
+    public function getSchemaOptions(): int
+    {
+        return $this->schemaOptions;
+    }
+}
---
+++ src/Index/RedisStorage.php
@@ -0,0 +1,186 @@
+<?php
+
+/**
+ * This file is part of the PHPhind package.
+ *
+ * (c) Elías Fernández Velázquez
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PHPhinder\Index;
+
+use Doctrine\DBAL\ArrayParameterType;
+use PHPhinder\Exception\StorageException;
+use PHPhinder\Schema\DefaultSchema;
+use PHPhinder\Schema\Schema;
+use PHPhinder\Token\RegexTokenizer;
+use PHPhinder\Token\Tokenizer;
+use PHPhinder\Utils\StringHelper;
+use PHPhinder\Utils\TypoTolerance;
+use Toflar\StateSetIndex\Levenshtein;
+
+class RedisStorage extends AbstractStorage implements Storage
+{
+    /** @var RedisIndex  */
+    protected Index $state;
+
+    /** @var RedisIndex */
+    protected Index $docs;
+
+    /** @var array<string, RedisIndex> */
+    protected array $indices = [];
+
+    public function __construct(
+        private readonly string $connectionString,
+        Schema $schema = new DefaultSchema(),
+        Tokenizer $tokenizer = new RegexTokenizer()
+    ) {
+        $this->docs = new RedisIndex($connectionString, sprintf('%s:%s', StringHelper::getShortClass($schema::class), 'docs'));
+        $this->state = new RedisIndex($connectionString, sprintf('%s:%s', StringHelper::getShortClass($schema::class), 'states'));
+
+        parent::__construct($schema, $tokenizer);
+
+        /**
+         * @var string $name
+         * @var int $options
+         */
+        foreach ($this->schemaVariables as $name => $options) {
+            if ($name === self::ID) {
+                throw new StorageException(sprintf('The schema provided contains a property with the reserved name %s', self::ID));
+            }
+            if ($options & Schema::IS_INDEXED) {
+                $this->indices[$name] = new RedisIndex(
+                    $this->connectionString,
+                    sprintf('%s:%s', StringHelper::getShortClass($schema::class), $name),
+                    $options
+                );
+            }
+        }
+    }
+
+    public function initialize(): void
+    {
+        $this->docs->create(array_merge([self::ID], array_keys(
+            array_filter($this->schemaVariables, fn ($var) => boolval($var & Schema::IS_STORED))
+        )));
+        $this->state->create([self::STATE]);
+
+        foreach ($this->indices as $index) {
+            $columns = [self::KEY, 'ids', self::STATE];
+            if ($index->getSchemaOptions() & Schema::IS_UNIQUE) {
+                unset($columns[2]);
+            }
+            $index->create($columns);
+        }
+    }
+
+
+    public function open(array $opts = []): void
+    {
+        foreach ($this->indices as $index) {
+            $index->open();
+        }
+        $this->docs->open();
+    }
+
+    public function count(): int
+    {
+        return $this->docs->getTotal();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function saveStates(array $new, array $deleted): void
+    {
+        if (count($new) > 0) {
+            $this->state->insertMultiple(
+                array_map(fn($state) => [RedisStorage::STATE => $state], $new)
+            );
+        }
+
+        if (count($deleted) > 0) {
+            $this->state->deleteMultiple(
+                array_map(fn($state) => [RedisStorage::STATE => $state], $deleted)
+            );
+        }
+    }
+
+    /**
+     * @param RedisIndex $index
+     * @return array<string, int|float|bool|string>
+     */
+    protected function load(Index $index, array $search): array
+    {
+        return $index->find($search);
+    }
+
+    /**
+     * @param RedisIndex $index
+     */
+    protected function loadPrefix(Index $index, array $search): \Generator
+    {
+        $search[key($search)] = current($search) . '%';
+        return $index->findAll($search);
+    }
+
+    /**
+     * @param RedisIndex $index
+     */
+    protected function loadAll(Index $index): \Generator
+    {
+        return $index->findAll();
+    }
+
+    /**
+     * @inheritDoc
+     * @param RedisIndex $index
+     */
+    protected function loadByStates(Index $index, array $states): \Generator
+    {
+        $keys = [];
+        foreach ($this->state->findAll([self::STATE => $states]) as $value) {
+            unset($value[self::STATE]);
+            if (0 === count($value)) {
+                continue;
+            }
+            $keys = array_merge($keys, array_keys($value));
+        }
+
+        if (0 === count($keys)) {
+            return [];
+        }
+
+        foreach ($keys as $key) {
+            $indexTerm = $index->find([self::KEY => $key]);
+            if ([] === $indexTerm) {
+                continue;
+            }
+            yield $indexTerm;
+        }
+    }
+
+    /**
+     * @inheritDoc
+     * @param RedisIndex $index
+     */
+    protected function save(Index $index, array $search, array $data, ?callable $hitCallback = null): void
+    {
+        if (in_array($index, $this->indices, true) && isset($data[self::STATE])) {
+            $this->state->set([self::STATE => $data[self::STATE]], [$data[self::KEY] => true]);
+            unset($data[self::STATE]);
+        }
+        $index->set($search, $data);
+    }
+
+    /**
+     * @inheritDoc
+     * @param RedisIndex $index
+     */
+    protected function remove(Index $index, array $search): void
+    {
+        $index->delete($search);
+    }
+}
